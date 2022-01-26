f052747bd78b94d91329ac0af35b60273e70ab4a
CocoAori <japansyj@gmail.com>
2022-01-26 16:24:30 +0000

    first commit 2

--- README.md
+++ README.md
@@ -23,3 +23,4 @@ Things you may want to cover:
 
 * ...
 # rails6-test
+# test 
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+	root "posts#index"
+  get 'posts/index'
+  get 'posts/new'
+  get 'posts/create'
+  get 'posts/show'
+  get 'posts/edit'
+  get 'posts/update'
+  get 'posts/destroy'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
