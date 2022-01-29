1d1ac98e0a0f8c6083aeb6953958461bdb17a4a7
CocoAori <japansyj@gmail.com>
2022-01-29 03:38:47 +0000

    first commit 8

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,35 @@
 class PostsController < ApplicationController
+	protect_from_forgery :only => ["create"]
+	
   def index
+	  @posts = Post.all
   end
 
   def new
   end
 
   def create
+	  post=Post.create(title: params[:title], content: params[:content])
+	  redirect_to "/posts/show/#{post.id}"
   end
 
   def show
+	  @post = Post.find(params[:id])
   end
 
   def edit
+	  @post = Post.find(params[:ids])
   end
 
   def update
+	  post = Post.find(params[:id])
+	  post.update(title: params[:title], content: params[:content])
+	  redirect_to "/posts/show/#{post.id}"
   end
 
   def destroy
+	  post = Post.find(params[:id])
+	  post.destroy
+	  redirect_to "/posts/index"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
 	root "posts#index"
   get 'posts/index'
-  get 'posts/new'
-  get 'posts/create'
-  get 'posts/show'
-  get 'posts/edit'
-  get 'posts/update'
-  get 'posts/destroy'
+  get 'posts/new' => "posts#new"
+  post 'posts/create' => "posts#create"
+  get 'posts/show/:id' => "posts#show"
+  get 'posts/edit/:ids' => "posts#edit"
+  post 'posts/update/:id' => "posts#update"
+  get 'posts/destroy/:id' => "posts#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
