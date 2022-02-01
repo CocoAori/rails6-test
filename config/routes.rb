ea3a83e44fa826efd931d8670498f123c9872e5b
CocoAori <japansyj@gmail.com>
2022-02-01 15:20:43 +0000

    first commit 10

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get 'posts/show/:id' => "posts#show"
   get 'posts/edit/:ids' => "posts#edit"
   post 'posts/update/:id' => "posts#update"
-  get 'posts/destroy/:id' => "posts#destroy"
+  #get 'posts/destroy/:id' => "posts#destroy"
+	delete 'posts/destroy/:id' => "posts#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
